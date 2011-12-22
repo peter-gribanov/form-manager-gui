b77a5adc322b5e3f1edffb10d810ead65bd64530
wcode404@gmail.com <wcode404@gmail.com@a2bb0ed2-66b6-051f-11cd-a2c32695401d>
2011-12-22 17:36:48 +0000

    добавил интерфейс хранилища

---
+++ branches/2.0/FormManager/Storage/Interface.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * FormManagerGUI package
+ * 
+ * @package   FormManagerGUI
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @version   2.0 SVN: $Revision: 41 $
+ * @since     $Date: 2011-10-01 00:28:31 +0400 (Сб, 01 окт 2011) $
+ * @link      http://peter-gribanov.ru/open-source/form-manager/4.0/
+ * @copyright 2008 by Peter Gribanov
+ * @license   http://peter-gribanov.ru/license	GNU GPL Version 3
+ */
+
+/**
+ * Интерфейс хранилища
+ * 
+ * @package FormManagerGUI\Storage
+ * @author  Peter Gribanov <info@peter-gribanov.ru>
+ */
+interface FormManager_Storage_Interface {
+
+	/**
+	 * Загружает форму
+	 * 
+	 * @return string $name Название формы
+	 * 
+	 * @return FormManager_Model_Form
+	 */
+	public function load($name);
+
+	/**
+	 * Сохраняет форму
+	 * 
+	 * @param FormManager_Model_Form $form Объект формы
+	 * 
+	 * @return boolean
+	 */
+	public function save(FormManager_Model_Form $form);
+
+	/**
+	 * Удаляет форму
+	 * 
+	 * @param string $name Название формы
+	 * 
+	 * @return boolean
+	 */
+	public function remove($name);
+
+	/**
+	 * Возвращает список всех форм
+	 * 
+	 * @return array
+	 */
+	public function getList();
+}
